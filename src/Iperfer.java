678f5c80e58c5728492aa56ec6b09efe8bf8d613
DANE JACOBSON <jacobsondane@icloud.com>
2025-02-04 17:01:21 -0600

    makefile 2

--- src/Iperfer.java
+++ src/Iperfer.java
@@ -3,6 +3,9 @@ public class Iperfer {
  
  
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
